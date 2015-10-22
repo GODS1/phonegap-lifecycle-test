def028da9565c3d6c9cdf4169ec17f3ce338996c
GODS1 <GODS1_15@uni.worc.ac.uk>
2015-10-22 16:39:06 +0100

    Added key variables
    yeah

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,15 @@
 var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
+var key = “pen”; 
+var value = “blue”; 
+
 
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
+		window.localStorage.setItem( key, value );
+		var key = “pen”;
+		var value = window.localStorage.getItem(key);
 		console.log("device ready");
     }
 	
@@ -39,3 +45,4 @@ var launched_count = 0;
 		resumed_count++;
 		updateDisplay();
     }
+
