944600098998c3605eaa24d94290b0fea5fcfa47
GODS1 <GODS1_15@uni.worc.ac.uk>
2015-10-22 17:48:27 +0100

    dgjhtg
    teyjhty

--- www/js/index.js
+++ www/js/index.js
@@ -2,14 +2,15 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 var key = “pen”; 
-var value = “blue”; 
+var value = “blue”;  
 
 
 
 
-    function onLoad() {
-var Flake = {
+function onLoad() {
+var Flake = [
 "Name":"Flake", "Energy":"2230 kJ", "Fat":"30.5g", "Carbohydrates":"56g", "Fibre":"0.7g", "Protein":"8.1g", "Salt":"0.28"}
+];
 Flake{0}.Name;
-    }
+    
 
