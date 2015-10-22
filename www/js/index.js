75af228ba8947ba7bebee5ea6af4a07fa9a19425
GODS1 <GODS1_15@uni.worc.ac.uk>
2015-10-22 18:02:23 +0100

    gotta go fast
    dank memes

--- www/js/index.js
+++ www/js/index.js
@@ -8,9 +8,17 @@ var value = “blue”;
 
 
 function onLoad() {
-var Flake = [
-"Name":"Flake", "Energy":"2230 kJ", "Fat":"30.5g", "Carbohydrates":"56g", "Fibre":"0.7g", "Protein":"8.1g", "Salt":"0.28"}
-];
-Flake{0}.Name;
-    
+var ChocBar = {
+"Name":"Flake", 
+"Energy":"2230 kJ", 
+"Fat":"30.5g", 
+"Carbohydrates":"56g", 
+"Fibre":"0.7g", 
+"Protein":"8.1g", 
+"Salt":"0.28",
+};
 
+console.log(ChocBar.name);
+
+    
+js
